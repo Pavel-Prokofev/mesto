700290523e7ed61e87cc2fb36f33451cee3101e8
Pavel Prokofev <kirgadan@yandex.ru>
2022-11-07 03:21:36 +0300

    Проектная работа четыре, попытка два

--- scripts/index.js
+++ scripts/index.js
@@ -5,40 +5,32 @@ var popupElem = document.querySelector('.popup');
 var formElement = popupElem.querySelector('.popup__form');
 var popupCloseButton = popupElem.querySelector('.popup__close-button');
 var popupSaveButton = popupElem.querySelector('.popup__save-button');
-var popupInputName = popupElem.querySelector('.popup__name');
-var popupInputInfo = popupElem.querySelector('.popup__info');
+var popupInputName = popupElem.querySelector('.popup__text-box_type_name');
+var popupInputInfo = popupElem.querySelector('.popup__text-box_type_info');
 
+function OnOffPopup() {
+  popupElem.classList.toggle('popup_opened');
+}
 
-
-
-function clickOnTheProfileEditButton() {
-  popupElem.classList.add('popup_open');
+function clickOnTheprofileEditButton() {
   popupInputName.value = profileName.textContent;
   popupInputInfo.value = profileInfo.textContent;
 }
 
-function clickOnThePopupCloseButton() {
-  popupElem.classList.remove('popup_open');
-}
-
 function clickOnThePopupSaveButton(evt) {
   evt.preventDefault();
-  if (popupInputName.value && popupInputInfo.value) {
-    profileName.textContent = popupInputName.value;
-    profileInfo.textContent = popupInputInfo.value;
-    clickOnThePopupCloseButton();
-  }
-
+  profileName.textContent = popupInputName.value;
+  profileInfo.textContent = popupInputInfo.value;
+  OnOffPopup();
 }
 
-profileEditButton.addEventListener('click', clickOnTheProfileEditButton); 
+profileEditButton.addEventListener('click', () => {
+  OnOffPopup();
+  clickOnTheprofileEditButton();
+});
 
-popupCloseButton.addEventListener('click', clickOnThePopupCloseButton);
+popupCloseButton.addEventListener('click', OnOffPopup);
 
 popupSaveButton.addEventListener('click', clickOnThePopupSaveButton);
 
 formElement.addEventListener('submit', clickOnThePopupSaveButton);
-
-
-
-
